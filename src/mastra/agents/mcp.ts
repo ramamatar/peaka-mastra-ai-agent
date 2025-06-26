f0a0c995b5b71d3f2bd7ee5aa1af908d2caf85fc
ramamatar <166447147+ramamatar@users.noreply.github.com>
2025-06-26 16:02:21 +0300

    Add files via upload

---
+++ src/mastra/agents/mcp.ts
@@ -0,0 +1,30 @@
+import { createTool } from "@mastra/core";
+import { MCPClient } from "@mastra/mcp";
+
+
+export const mcp = new MCPClient({
+  servers: {
+    peaka: {
+      command: "npx",
+      args: ["-y", "@peaka/mcp-server-peaka@latest"],
+      env: {
+        PEAKA_API_KEY: process.env.PEAKA_API_KEY || "",
+      },
+    },
+  },
+});
+
+export const getSQLRuleSet = async () => {
+  const resources = await mcp.resources.list();
+
+  if (resources.peaka) {
+    const resource = resources.peaka.find(
+      (r) => r.uri === "file:///peaka_sql_query_rule_set.txt"
+    );
+    if (!resource) {
+      throw new Error("Resource not found");
+    }
+    console.log(resource);
+    const result = await mcp.resources.read("peaka", resource.uri);
+    return result.contents[0].text;
+  } }
